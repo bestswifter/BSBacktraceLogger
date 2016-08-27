91d22973ce051361dcef105cd104cc07efba8837
bestswifter <ktzhang@bestswifter.com>
2016-08-27 19:02:50 +0800

    Rename project from kt to bs

--- BsBacktraceLogger/ViewController.m
+++ BsBacktraceLogger/ViewController.m
@@ -30,8 +30,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-        BSLOG  // 打印当前线程的调用栈
-        BSLOG_ALL  // 打印所有线程的调用栈
         BSLOG_MAIN  // 打印主线程调用栈
         // 调用 [BsBacktraceLogger backtraceOfCurrentThread] 这一系列的方法可以获取字符串，然后选择上传服务器或者其他处理。
     });
