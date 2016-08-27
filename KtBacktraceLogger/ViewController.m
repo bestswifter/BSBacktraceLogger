a8ab2c5a50cde20e191dfa727ebd4db02cabb550
bestswifter <ktzhang@bestswifter.com>
2016-08-27 09:00:30 +0800

    Merge Third Party Code

--- KtBacktraceLogger/ViewController.m
+++ KtBacktraceLogger/ViewController.m
@@ -30,9 +30,13 @@
 
 #import <mach/mach.h>
 #include <dlfcn.h>
-#include "KSDynamicLinker.h"
+//#include "KSDynamicLinker.h"
 #include <pthread.h>
 #include <sys/types.h>
+#include <limits.h>
+#include <string.h>
+#include <mach-o/dyld.h>
+#include <mach-o/nlist.h>
 
 @interface ViewController ()
 
@@ -61,13 +65,13 @@ void ksbt_symbolicate(const uintptr_t* const backtraceBuffer,
     
     if(!skippedEntries && i < numEntries)
     {
-        ksdl_dladdr(backtraceBuffer[i], &symbolsBuffer[i]);
+        kt_dladdr(backtraceBuffer[i], &symbolsBuffer[i]);
         i++;
     }
     
     for(; i < numEntries; i++)
     {
-        ksdl_dladdr(CALL_INSTRUCTION_FROM_RETURN_ADDRESS(backtraceBuffer[i]), &symbolsBuffer[i]);
+        kt_dladdr(CALL_INSTRUCTION_FROM_RETURN_ADDRESS(backtraceBuffer[i]), &symbolsBuffer[i]);
     }
 }
 
@@ -217,7 +221,7 @@ int  backtraceThread(thread_t thread) {
     int backtraceLength = i;
     Dl_info symbolicated[backtraceLength];
     ksbt_symbolicate(backtraceBuffer, symbolicated, backtraceLength, 0);
-//    ksdl_dladdr(CALL_INSTRUCTION_FROM_RETURN_ADDRESS(backtraceBuffer[i]), &symbolicated[i]);
+//    kt_dladdr(CALL_INSTRUCTION_FROM_RETURN_ADDRESS(backtraceBuffer[i]), &symbolicated[i]);
 //    dladdr((void *)backtraceBuffer[i], &symbolicated[i]);
     for (int i = 0; i < backtraceLength; ++i) {
         kscrw_i_logBacktraceEntry(i, backtraceBuffer[i], &symbolicated[i]);
@@ -245,7 +249,6 @@ static void dumpThreads(void) {
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    getThreadsCount();
     [[NSThread currentThread] setName:@"main thread"];
     
     mach_port_t mach_thread = pthread_mach_thread_np(pthread_self());
@@ -262,13 +265,9 @@ static void dumpThreads(void) {
         const thread_t this_thread = mach_thread_self();
         backtraceThread(main_thread);
         backtraceThread(this_thread);
+        getThreadsCount();
     });
     [self foo];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (void)foo {
@@ -279,6 +278,148 @@ static void dumpThreads(void) {
     while (true) {
         ;
     }
+}
+
+#pragma -mark Symbolicate
+uintptr_t kt_firstCmdAfterHeader(const struct mach_header* const header) {
+    switch(header->magic) {
+        case MH_MAGIC:
+        case MH_CIGAM:
+            return (uintptr_t)(header + 1);
+        case MH_MAGIC_64:
+        case MH_CIGAM_64:
+            return (uintptr_t)(((struct mach_header_64*)header) + 1);
+        default:
+            return 0;  // Header is corrupt
+    }
+}
+
+uint32_t kt_imageIndexContainingAddress(const uintptr_t address) {
+    const uint32_t imageCount = _dyld_image_count();
+    const struct mach_header* header = 0;
+    
+    for(uint32_t iImg = 0; iImg < imageCount; iImg++) {
+        header = _dyld_get_image_header(iImg);
+        if(header != NULL) {
+            // Look for a segment command with this address within its range.
+            uintptr_t addressWSlide = address - (uintptr_t)_dyld_get_image_vmaddr_slide(iImg);
+            uintptr_t cmdPtr = kt_firstCmdAfterHeader(header);
+            if(cmdPtr == 0) {
+                continue;
+            }
+            for(uint32_t iCmd = 0; iCmd < header->ncmds; iCmd++) {
+                const struct load_command* loadCmd = (struct load_command*)cmdPtr;
+                if(loadCmd->cmd == LC_SEGMENT) {
+                    const struct segment_command* segCmd = (struct segment_command*)cmdPtr;
+                    if(addressWSlide >= segCmd->vmaddr &&
+                       addressWSlide < segCmd->vmaddr + segCmd->vmsize) {
+                        return iImg;
+                    }
+                }
+                else if(loadCmd->cmd == LC_SEGMENT_64) {
+                    const struct segment_command_64* segCmd = (struct segment_command_64*)cmdPtr;
+                    if(addressWSlide >= segCmd->vmaddr &&
+                       addressWSlide < segCmd->vmaddr + segCmd->vmsize) {
+                        return iImg;
+                    }
+                }
+                cmdPtr += loadCmd->cmdsize;
+            }
+        }
+    }
+    return UINT_MAX;
+}
+
+uintptr_t kt_segmentBaseOfImageIndex(const uint32_t idx) {
+    const struct mach_header* header = _dyld_get_image_header(idx);
+    
+    // Look for a segment command and return the file image address.
+    uintptr_t cmdPtr = kt_firstCmdAfterHeader(header);
+    if(cmdPtr == 0) {
+        return 0;
+    }
+    for(uint32_t i = 0;i < header->ncmds; i++) {
+        const struct load_command* loadCmd = (struct load_command*)cmdPtr;
+        if(loadCmd->cmd == LC_SEGMENT) {
+            const struct segment_command* segmentCmd = (struct segment_command*)cmdPtr;
+            if(strcmp(segmentCmd->segname, SEG_LINKEDIT) == 0) {
+                return segmentCmd->vmaddr - segmentCmd->fileoff;
+            }
+        }
+        else if(loadCmd->cmd == LC_SEGMENT_64) {
+            const struct segment_command_64* segmentCmd = (struct segment_command_64*)cmdPtr;
+            if(strcmp(segmentCmd->segname, SEG_LINKEDIT) == 0) {
+                return (uintptr_t)(segmentCmd->vmaddr - segmentCmd->fileoff);
+            }
+        }
+        cmdPtr += loadCmd->cmdsize;
+    }
+    return 0;
+}
+
+bool kt_dladdr(const uintptr_t address, Dl_info* const info) {
+    info->dli_fname = NULL;
+    info->dli_fbase = NULL;
+    info->dli_sname = NULL;
+    info->dli_saddr = NULL;
+    
+    const uint32_t idx = kt_imageIndexContainingAddress(address);
+    if(idx == UINT_MAX) {
+        return false;
+    }
+    const struct mach_header* header = _dyld_get_image_header(idx);
+    const uintptr_t imageVMAddrSlide = (uintptr_t)_dyld_get_image_vmaddr_slide(idx);
+    const uintptr_t addressWithSlide = address - imageVMAddrSlide;
+    const uintptr_t segmentBase = kt_segmentBaseOfImageIndex(idx) + imageVMAddrSlide;
+    if(segmentBase == 0) {
+        return false;
+    }
+    
+    info->dli_fname = _dyld_get_image_name(idx);
+    info->dli_fbase = (void*)header;
+    
+    // Find symbol tables and get whichever symbol is closest to the address.
+    const struct nlist_64* bestMatch = NULL;
+    uintptr_t bestDistance = ULONG_MAX;
+    uintptr_t cmdPtr = kt_firstCmdAfterHeader(header);
+    if(cmdPtr == 0) {
+        return false;
+    }
+    for(uint32_t iCmd = 0; iCmd < header->ncmds; iCmd++) {
+        const struct load_command* loadCmd = (struct load_command*)cmdPtr;
+        if(loadCmd->cmd == LC_SYMTAB) {
+            const struct symtab_command* symtabCmd = (struct symtab_command*)cmdPtr;
+            const struct nlist_64* symbolTable = (struct nlist_64*)(segmentBase + symtabCmd->symoff);
+            const uintptr_t stringTable = segmentBase + symtabCmd->stroff;
+            
+            for(uint32_t iSym = 0; iSym < symtabCmd->nsyms; iSym++) {
+                // If n_value is 0, the symbol refers to an external object.
+                if(symbolTable[iSym].n_value != 0) {
+                    uintptr_t symbolBase = symbolTable[iSym].n_value;
+                    uintptr_t currentDistance = addressWithSlide - symbolBase;
+                    if((addressWithSlide >= symbolBase) &&
+                       (currentDistance <= bestDistance)) {
+                        bestMatch = symbolTable + iSym;
+                        bestDistance = currentDistance;
+                    }
+                }
+            }
+            if(bestMatch != NULL) {
+                info->dli_saddr = (void*)(bestMatch->n_value + imageVMAddrSlide);
+                info->dli_sname = (char*)((intptr_t)stringTable + (intptr_t)bestMatch->n_un.n_strx);
+                if(*info->dli_sname == '_') {
+                    info->dli_sname++;
+                }
+                // This happens if all symbols have been stripped.
+                if(info->dli_saddr == info->dli_fbase && bestMatch->n_type == 3) {
+                    info->dli_sname = NULL;
+                }
+                break;
+            }
+        }
+        cmdPtr += loadCmd->cmdsize;
+    }
+    return true;
 }
 
 @end
