06a35c6bc88be3ecc707774e415246fad407f06a
bestswifter <ktzhang@bestswifter.com>
2016-08-27 19:05:41 +0800

    Rename missing stuff

--- BsBacktraceLogger/AppDelegate.m
+++ BsBacktraceLogger/AppDelegate.m
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.m
-//  KtBacktraceLogger
+//  BsBacktraceLogger
 //
 //  Created by 张星宇 on 16/8/26.
 //  Copyright © 2016年 bestswifter. All rights reserved.
--- BsBacktraceLogger/BsBacktraceLogger.h
+++ BsBacktraceLogger/BsBacktraceLogger.h
@@ -1,6 +1,6 @@
 //
-//  KtBackTraceLogger.h
-//  KtBacktraceLogger
+//  BsBackTraceLogger.h
+//  BsBacktraceLogger
 //
 //  Created by 张星宇 on 16/8/27.
 //  Copyright © 2016年 bestswifter. All rights reserved.
