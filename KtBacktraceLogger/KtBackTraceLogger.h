45bc7d7caadf17a95eb3ecf188e36cd5d8430889
bestswifter <ktzhang@bestswifter.com>
2016-08-27 09:42:44 +0800

    Separate Code into KtBacktraceLogger

---
+++ KtBacktraceLogger/KtBackTraceLogger.h
@@ -0,0 +1,15 @@
+//
+//  KtBackTraceLogger.h
+//  KtBacktraceLogger
+//
+//  Created by 张星宇 on 16/8/27.
+//  Copyright © 2016年 bestswifter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KtBackTraceLogger : NSObject
+
++ (void)printBackTraceOfAllThread;
+
+@end
